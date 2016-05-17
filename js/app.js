4a2512ee31b5c9941e08266a8a986a7686780018
jerry <Jerry@guojings-MacBook-Pro.local>
2016-05-17 08:46:44 -0500

    basic function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies
     this.sprite = 'images/enemy-bug.png';
+    this.startX = -101;
+    this.endX = 505;
+    this.lanes = [73, 156, 239]; //three lanes
+    this.reset();
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+Enemy.prototype.reset = function() {
+    this.x = this.startX;
+    this.y = this.lanes[Math.floor(Math.random()*3)];//get on random y
+    this.speed = 100 + Math.ceil(Math.random()*300); //random speed at range 101 - 400
+};
+
+// Update the enemy's position using dt
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  var maxPos = this.endX;
+  //right-most
+  if (this.x <= maxPos){
+    this.x += this.speed * dt;
+  }
+  else{
+    this.reset();
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +31,52 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// player class
+// update(), render() and handleInput() method.
 
+var Player = function() {
+    this.xRange = [0, 404];
+    this.yRange = [-10, 405];
+    this.sprite = 'images/char-boy.png';
+    this.reset();
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.reset = function() {
+    this.x = 202;
+    this.y = 405; //start from -10,
+};
 
+Player.prototype.update = function() {
+  //when reach to the water,
+    if (this.y == -10) {
+      // alert("Awsome"); Need prompt a succeed window.
+      this.reset();
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+
+    if (key === 'left' && this.x>=101) {
+        this.x -= 101;
+    } else if (key === 'right' && this.x < 404) {
+        this.x += 101;
+    } else if (key === 'up' && this.y >= 73) {
+        this.y -= 83;
+    } else if (key === 'down' && this.y < 405) {
+        this.y += 83;
+    }
+};
+
+// Now instantiate your objects. allEnemies and player
+var e1 = new Enemy();
+var e2 = new Enemy();
+var e3 = new Enemy();
+var allEnemies = [e1,e2,e3];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
